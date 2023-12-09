a1c0835ee6f67ea90ccf681b3365a63b209f3455
xXpeira12 <78487961+xXpeira12@users.noreply.github.com>
2023-12-09 20:32:56 +0700

    update contact

--- src/sections/Contact/index.jsx
+++ src/sections/Contact/index.jsx
@@ -3,7 +3,7 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const Contact = () =>{
     return(
-        <div className='flex items-end text-2xl'>
+        <div className='flex items-end text-2xl mt-36'>
             <a href="https://github.com/xXpeira12" target="_blank">
                 <FontAwesomeIcon className='transition-all hover:scale-125 hover:text-primaryTitle' icon={faGithub}></FontAwesomeIcon>
             </a>  
